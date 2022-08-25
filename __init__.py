8251033bf5a8510ce4288d658dca52dcf4547327
Stefano Garcia <stefano.rgc@gmail.com>
2022-08-25 11:44:43 +0200

    Functional

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# print('__init__.py is read :-)')
+# from . import get_images
+import tessutils2.get_images
+import tessutils2.get_lc_summary_from_groups2
+import tessutils2.get_lcs_from_groups2
+import tessutils2.group_results2
+import tessutils2.LC_Extractor3
+import tessutils2.utils
+# # import tessutils.get_lc_summary_from_groups
+# import tessutils.get_lcs_from_groups
+# import tessutils.group_results
+# import tessutils.LC_Extractor
